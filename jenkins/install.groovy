c4c2502148c67aadc03429a8aa4c781a1d8ec4af
Mārtiņš J <martinsjakans99@gmail.com>
2025-04-02 20:57:09 +0300

    w

--- jenkins/install.groovy
+++ jenkins/install.groovy
@@ -1,6 +1,6 @@
 def call(String workingDir) {
     dir(workingDir) {
-        echo "Instalē Python bibliotēkas..."
+        echo ">>> TESTĒJAM, VAI IZMANTO JAUNO install.groovy"
         bat 'powershell -Command "& \\"C:\\Users\\Dell\\AppData\\Local\\Programs\\Python\\Python313\\Scripts\\pip.exe\\" install -r requirements.txt"'
     }
 }
