b28f59c52eb78d5408edb4638a1dffbb8a285e02
Mārtiņš J <martinsjakans99@gmail.com>
2025-04-03 15:02:16 +0300

    we

--- jenkins/deploy.groovy
+++ jenkins/deploy.groovy
@@ -1,9 +1,9 @@
 def deploy(envName, port) {
     echo ">>> Using deploy.groovy for environment: ${envName}"
     dir('.') {
-        bat '"C:\\Users\\Dell\\AppData\\Roaming\\npm\\pm2.cmd" delete greetings-app-${envName} || exit 0'
-        bat '"C:\\Users\\Dell\\AppData\\Roaming\\npm\\pm2.cmd" start app.py --name greetings-app-${envName} --interpreter python -- --port ${port}'
-        bat '"C:\\Users\\Dell\\AppData\\Roaming\\npm\\pm2.cmd" list'
+        bat("""C:\\Users\\Dell\\AppData\\Roaming\\npm\\pm2.cmd delete greetings-app-${envName} || exit 0""")
+        bat("""C:\\Users\\Dell\\AppData\\Roaming\\npm\\pm2.cmd start app.py --name greetings-app-${envName} --interpreter C:\\Users\\Dell\\AppData\\Local\\Programs\\Python\\Python313\\python.exe -- --port ${port}""")
+        bat("""C:\\Users\\Dell\\AppData\\Roaming\\npm\\pm2.cmd list""")
     }
 }
 return this
