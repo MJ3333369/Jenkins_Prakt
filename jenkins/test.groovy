b3ea218ad987f3263ad352dda36cd7eae19914e1
Mārtiņš J <martinsjakans99@gmail.com>
2025-04-03 14:28:32 +0300

    tuks

--- jenkins/test.groovy
+++
@@ -1,9 +0,0 @@
-def runTests(envTag) {
-    echo "Instalē Node.js bibliotēkas testēšanai..."
-    bat 'npm install'
-
-    echo "Veic testus pret vidi: ${envTag}..."
-    bat "npm run greetings ${envTag}"
-}
-
-return this
